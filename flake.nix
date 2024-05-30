03f829e3faa96eb9c5d50128478c5e2c270e67d3
StarrFox <StarrFox6312@gmail.com>
2024-05-30 16:00:35 -0500

    update dependencies

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Wizard101 scripting library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts/";
+    nix-systems.url = "github:nix-systems/default";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nix-systems,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      debug = true;
+      systems = import nix-systems;
+      perSystem = {
+        pkgs,
+        system,
+        self',
+        ...
+      }: let
+        python = pkgs.python312;
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              black.enable = true;
+              alejandra.enable = true;
+              statix.enable = true;
+              typos.enable = true;
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "wizwalker";
+          inherit (self'.checks.pre-commit-check) shellHook;
+          packages = with pkgs; [
+            (poetry.withPlugins (ps: with ps; [poetry-plugin-up]))
+            python
+            just
+            alejandra
+            python.pkgs.black
+            python.pkgs.isort
+            python.pkgs.python-lsp-server
+            python.pkgs.mypy
+          ];
+        };
+      };
+    };
+}
